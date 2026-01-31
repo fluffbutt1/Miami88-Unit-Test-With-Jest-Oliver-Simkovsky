a7d9d9650872893205900333fa9916d56e86a84b
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-01-31 01:54:54 +0000

    need to finish final functions

---
+++ .learn/resets/11-homework/app.js
@@ -0,0 +1,17 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const 
+
+const sum = (a,b) => {
+    return a + b;
+}
+
+console.log(sum(7,3));
+
+module.exports = { sum, fromEuroToDollar };
---
+++ app.js
@@ -0,0 +1,20 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar){
+    let valueInYen = valueInDollar * 154.74;
+    return valueInYen;
+}
+
+const sum = (a,b) => {
+    return a + b;
+}
+
+console.log(sum(7,3));
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen};
